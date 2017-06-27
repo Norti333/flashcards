07e58fb56ad75f89d15a238e13c7c0854bdbbaeb
LeighBarnett <leighbarnett5@gmail.com>
2017-06-27 13:23:21 +0300

    schemata defined, testing db

---
+++ models/cardModel.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+
+var Schema=mongoose.Schema;
+
+var cardSchema = new Schema({
+front: String,
+back: String,
+hint: String,
+level: Number,
+deck: {type:Schema.Types.ObjectId,ref:'deck'}
+});
+
+var Card = mongoose.model('card', cardSchema);
+
+var card1= new Card({
+front: "3 x 3",
+back: "9",
+hint: "count 3 3 times!",
+level:1
+});
+
+var card2= new Card({
+front: "4 x 4",
+back: "16",
+hint: "count 4 4 times!",
+level:2
+});
+
+var card3= new Card({
+front: "5 x 5",
+back: "25",
+hint: "count 5 5 times!",
+level:2
+});
+
+var card4= new Card({
+front: "6 x 6",
+back: "36",
+hint: "count 6 6 times!",
+level:3
+});
+
+var card5= new Card({
+front: "2 x 2",
+back: "4",
+hint: "count 2 2 times!",
+level:2,
+deck: deckModel.js\deck1
+});
+
+
+// card1.save()
+// card2.save()
+// card3.save()
+// card4.save()
+card5.save()
+module.exports = Card;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/spacebookDB', function () {
+mongoose.connect('mongodb://localhost/digitalflashDB', function () {
   console.log("DB connection established!!!");
 })
+
+var Card = require("./models/cardModel")
+var Deck = require("./models/deckModel")
+
+
 
 var app = express();
 app.use(express.static('public'));
@@ -16,5 +20,5 @@ app.use(bodyParser.urlencoded({
 
 
 app.listen(8000, function () {
-  console.log("Get me on 8000 ;-)");
+  console.log('Server up and running on port 3000 ;-)');
 });
