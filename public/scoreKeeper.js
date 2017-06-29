f9b6fa8f73aae4078ee50297b7d1e5494e7cc3e9
LeighBarnett <leighbarnett5@gmail.com>
2017-06-29 12:01:41 +0300

    started counter function

---
+++ public/scoreKeeper.js
@@ -0,0 +1,20 @@
+attemptsCounter = 0;
+scoreCounter = 0;
+
+//when click 'try', attemptsCounter ++
+
+for (var i = 0; i < cards.length; i++) {
+    for (cards[i]) {
+        if (attemptsCounter === 1 && tryValue == backText) {
+            scoreCounter += 5;
+        } else if (attemptsCounter === 2 && tryValue == backText) {
+            scoreCounter += 3;
+        } else if (attemptsCounter === 3 && tryValue == backText) {
+            scoreCounter += 1;
+        } else if (attemptsCounter > 3 && tryValue != backText) {
+            console.log("The answer is " + backText);
+            $('.tryText').hide();
+        }
+        attemptsCounter = 0
+    }
+}
